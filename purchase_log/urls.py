0f06d1c5ac686ad326261b3560b11a1da8d3e23e
shodai <show.0417@icloud.com>
2025-01-18 04:36:42 +0900

    lineのurlパスを削除

--- purchase_log/urls.py
+++ purchase_log/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('check_user/', views.check_user, name='check_user'),
     path('create_purchase_log/', views.create_purchase_log, name='create_purchase_log'),
-    path('send_to_line_group/', views.send_to_line_group, name='send_to_line_group'),
 ]

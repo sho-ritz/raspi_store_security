6934e1a548fd36d54cfcd009b4e8da33bbc9abc0
shodai <show.0417@icloud.com>
2025-01-19 02:17:03 +0900

    HTTPレスポンス用のライブラリをインポート

--- purchase_log/views.py
+++ purchase_log/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST

a2cab6369dc66e5146454df9805002daf7e4cf8a
shodai <show.0417@icloud.com>
2025-01-19 00:52:15 +0900

    webhookの実装

--- purchase_log/urls.py
+++ purchase_log/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('check_user/', views.check_user, name='check_user'),
     path('create_purchase_log/', views.create_purchase_log, name='create_purchase_log'),
+    path('webhook/', views.line_webhook, name='webhook'),
 ]
--- purchase_log/views.py
+++ purchase_log/views.py
@@ -56,13 +56,41 @@ class PurchaseLogListView(generics.ListAPIView):
     serializer_class = serializers.PurchaseLogSerializer
     permission_classes = [IsAuthenticated]
 
-# LINE Messaging APIのアクセストークン
+# LINE系の環境変数を取得
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_GROUP_ID = os.getenv("LINE_GROUP_ID")
-
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
-def send_to_line_group(status, user_name="", item_name="", price=0, time="00:00", error_message=""):
+@csrf_exempt
+def line_webhook(request):
+    if request.method == "POST":
+        signature = request.headers.get("X-Line-Signature", "")
+        body = request.body.decode("utf-8")
+
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            return HttpResponse("Invalid signature", status=403)
+
+        return HttpResponse("OK")
+    else:
+        return HttpResponse("Method not allowed", status=405)
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+
+    user_message = event.message.text
+
+    if user_message == "@在庫確認":
+        on_sale_items = models.Item.objects.filter(is_sales=True)
+
+        line_bot_api.reply_message(
+            LINE_GROUP_ID,
+            TextSendMessage(text="\n".join([f"{50*(i+1)}円商品: {item.name}, 在庫数: {item.stock}" for i, item in enumerate(on_sale_items)]))
+        )
+
+def send_to_line_group(status, user_name="不明なユーザー", item_name="不明な商品", price=0, time="00:00", error_message=""):
     if status == 200:
         message = f"{time}: {user_name}さんが{item_name}を{price}円で購入しました。"
     elif status == 400:
@@ -70,17 +98,14 @@ def send_to_line_group(status, user_name="", item_name="", price=0, time="00:00"
     else:
         message = f"{time}: {error_message}というエラーが発生しました。"
 
-    # LINEグループにメッセージを送信
     line_bot_api.push_message(
         LINE_GROUP_ID, TextSendMessage(text=message)
     )
 
 def hex_to_shiftjis(hex_string):
     try:
-        # 16進数文字列をバイトデータに変換
         byte_data = bytes.fromhex(hex_string)
 
-        # バイトデータをShift_JIS文字列にデコード
         shiftjis_string = byte_data.decode('shift_jis')
 
         return shiftjis_string
@@ -124,7 +149,7 @@ def create_purchase_log(request):
 
         item = models.Item.objects.filter(price=price, is_sales=True).first()
 
-        if item:  # 結果が存在するか確認
+        if item:
             item_id = item.id
         else:
             error_message = '商品が売り切れ、もしくは未販売'
